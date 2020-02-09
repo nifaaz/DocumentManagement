86d79fd5ec4d0a9e0e0c90dbaebe92866a99ddaf
Le Kim Phi <48780111+KimPhiJSCDVLP@users.noreply.github.com>
2020-02-09 10:52:56 +0700

    SH_07: Add class common status for project.

---
+++ DocumentManagement/Common/CommonConst.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.Common
+{
+    public class CommonConst
+    {
+        public const int QUEUE = 1;
+        public const int IN_PROCESSS = 2;
+        public const int FINISH = 3;
+        public const int DISABLE = 4;
+        public const int ENABLE = 5;
+    }
+}
