27b4d98b23a1ae9c543184dbf90fbb40cddcd99f
Le Kim Phi <48780111+KimPhiJSCDVLP@users.noreply.github.com>
2020-02-13 21:47:40 +0700

    SH_09: Add APi Address.

---
+++ DocumentManagement/BUS/AddressBUS.cs
@@ -0,0 +1,71 @@
+﻿using DocumentManagement.Common;
+using DocumentManagement.DAL;
+using DocumentManagement.Model.Entity.Address;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.BUS
+{
+    public class AddressBUS
+    {
+        private static AddressDAL addressDAL = AddressDAL.GetAddressDALInstance;
+        private AddressBUS() { }
+
+        private static volatile AddressBUS _instance;
+
+        static object key = new object();
+
+        public static AddressBUS GetAddressBUSInstance
+        {
+            get
+            {
+                lock (key)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new AddressBUS();
+                    }
+                }
+
+                return _instance;
+            }
+
+            private set
+            {
+                _instance = value;
+            }
+        }
+        //public ReturnResult<Organ> GetPagingWithSearchResults(BaseCondition<Organ> condition)
+        //{
+        //    var result = organDAL.GetPagingWithSearchResults(condition);
+        //    return result;
+        //}
+        //public ReturnResult<Organ> GetAllOrgan()
+        //{
+        //    var result = organDAL.GetAllOrgan();
+        //    return result;
+        //}
+        //public ReturnResult<Organ> GetOrganByID(int OrganID)
+        //{
+        //    var rs = organDAL.GetOrganByID(OrganID);
+        //    return rs;
+        //}
+        public ReturnResult<Provincial> GetAllTinh()
+        {
+            var rs = addressDAL.GetAllTinh();
+            return rs;
+        }
+        public ReturnResult<District> GetHuyenByTinhID(int tinhID)
+        {
+            var rs = addressDAL.GetHuyenByTinhID(tinhID);
+            return rs;
+        }
+        public ReturnResult<Wards> GetXaPhuongByHuyenID(int huyenID)
+        {
+            var rs = addressDAL.GetXaPhuongByHuyenID(huyenID);
+            return rs;
+        }
+    }
+}
