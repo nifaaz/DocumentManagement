ed45bbdb4ee32124bdb8eaa5a8a0e55a7221ce84
Nguyễn Duy Tú <tunguyenduy@tunguyenduys-MacBook.local>
2020-02-27 13:30:26 +0700

    commit

---
+++ DocumentManagement/Models/Entity/Organ/OrganList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DocumentManagement.Models.Entity.Organ
+{
+    public class OrganList
+    {
+        public List<string> OrganTypes { get; set; } = new List<string>();
+        public List<string> OrganName { get; set; } = new List<string>();
+        public List<string> OrganAddress { get; set; } = new List<string>();
+    }
+}
+
