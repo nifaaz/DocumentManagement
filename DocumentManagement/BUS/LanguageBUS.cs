695e83ca2246b0f25ea6371434e5daab99670ac5
Nam <nhatnamdev98@gmail.com>
2020-03-09 10:38:20 +0700

    push code

---
+++ DocumentManagement/BUS/LanguageBUS.cs
@@ -0,0 +1,28 @@
+﻿using DocumentManagement.Common;
+using DocumentManagement.Models.Entity.Language;
+using LanguageManagement.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageManagement.BUS
+{
+    public class LanguageBUS
+    {
+        private LanguageDAL _languageDAL;
+        private LanguageDAL LanguageDAL
+        {
+            get
+            {
+                _languageDAL = new LanguageDAL();
+                return _languageDAL;
+            }
+        }
+        public ReturnResult<Language> GetAllLanguage()
+        {
+            var result = LanguageDAL.GetAllLanguage();
+            return result;
+        }
+    }
+}
