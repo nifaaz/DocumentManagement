9f82e9118558db08bffbf598ff9d35d2c7b399dd
Nam <nhatnamdev98@gmail.com>
2020-03-09 11:05:34 +0700

    create format branch

---
+++ DocumentManagement/DAL/FormatDAL.cs
@@ -0,0 +1,38 @@
+﻿using DocumentManagement.Common;
+using DocumentManagement.Model;
+using DocumentManagement.Models.Entity.Format;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.DAL
+{
+    public class FormatDAL
+    {
+        public ReturnResult<Format> GetAllFormat()
+        {
+            List<Format> documentList = new List<Format>();
+            DbProvider dbProvider = new DbProvider();
+            string outCode = String.Empty;
+            string outMessage = String.Empty;
+            int totalRows = 0;
+            dbProvider.SetQuery("FORMAT_GET_ALL", CommandType.StoredProcedure)
+                .SetParameter("ErrorCode", SqlDbType.NVarChar, DBNull.Value, 100, ParameterDirection.Output)
+                .SetParameter("ErrorMessage", SqlDbType.NVarChar, DBNull.Value, 255, ParameterDirection.Output)
+                .GetList<Format>(out documentList)
+                .Complete();
+            dbProvider.GetOutValue("ErrorCode", out outCode)
+                       .GetOutValue("ErrorMessage", out outMessage);
+
+            return new ReturnResult<Format>()
+            {
+                ItemList = documentList,
+                ErrorCode = outCode,
+                ErrorMessage = outMessage,
+                TotalRows = totalRows
+            };
+        }
+    }
+}
